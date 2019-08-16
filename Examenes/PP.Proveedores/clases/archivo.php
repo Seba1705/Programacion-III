5130c7e2c1521cc168469b595f983144ff718663
Sebastian Aguirre <sebaaguirre2012@gmail.com>
2019-08-16 05:27:27 -0300

    punto 1 pp prov

--- Examenes/PP.Proveedores/clases/archivo.php
+++ Examenes/PP.Proveedores/clases/archivo.php
@@ -1,10 +1,41 @@
 <?php
     class Archivo{
+
         public static function existePeticionPOST(){
             return $_SERVER['REQUEST_METHOD'] == 'POST' ? true : false;
         }
+
         public static function existePeticionGET(){
             return $_SERVER['REQUEST_METHOD'] == 'GET' ? true : false;
         }
+
+        public static function guardarUno( $path, $objeto ){
+            $archivo = fopen( $path, 'a+' );
+            fwrite( $archivo, $objeto->toCsv() );
+            fclose( $archivo );
+        }
+
+        public static function guardarTodos( $path, $lista ){
+            $archivo = fopen( $path, 'w' );
+            foreach( $lista as $objeto ){
+                fwrite( $archivo, $objeto->toCsv() );
+            }
+            fclose( $archivo );
+        }
+
+        public static function leerProveedores(){
+            $listaDeProveedores = array();
+            $archivo = fopen( './archivos/proveedores.txt', 'r' );
+            do{
+                $proveedor = trim( fgets($archivo) );
+                if( $proveedor != '' ){
+                    $proveedor = explode( ';', $proveedor );
+                    array_push( $listaDeProveedores, new Proveedor($proveedor[0], $proveedor[1], $proveedor[2], $proveedor[3]) );
+                }
+            }while( !feof($archivo) );
+            fclose( $archivo );
+            return $listaDeProveedores;
+        }
+        
     }
 ?>
--- Examenes/PP.Proveedores/clases/proveedor.php
+++ Examenes/PP.Proveedores/clases/proveedor.php
@@ -20,14 +20,31 @@
             return 'ID: ' .$this->id. ' NOMBRE: ' .$this->nombre. ' EMAIL: ' .$this->email. ' FOTO: ' .$this->foto. PHP_EOL;
         }
         
+        /////////////////////////////////////////////////////////////////
+        // CARGAR PROVEEDOR
+        /////////////////////////////////////////////////////////////////
+
         public static function cargarProveedor(){
             if( Archivo::existePeticionPOST() ){
                 if( isset($_POST['id']) && !empty($_POST['id']) && 
                     isset($_POST['nombre']) && !empty($_POST['nombre']) &&
                     isset($_POST['email']) && !empty($_POST['email']) &&
                     isset($_FILES['foto']) ){
-                    
-                    echo 'Proveedor cargado';
+                    if( Proveedor::existeID( $_POST['id']) ){
+                        //Acciones sobre foto
+                        $origen = $_FILES["foto"]["tmp_name"];
+                        $nombreOriginal = $_FILES["foto"]["name"];
+                        $ext = pathinfo($nombreOriginal, PATHINFO_EXTENSION);
+                        $destinoFoto = "./img/".$_POST['id'].".".$ext;
+                        move_uploaded_file($origen, $destinoFoto);
+
+                        $proveedor = new Proveedor( $_POST['id'], $_POST['nombre'], $_POST['email'], $destinoFoto );
+                        Archivo::guardarUno( './archivos/proveedores.txt', $proveedor );
+                        
+                        echo 'Se cargo el proveedor: ' . $proveedor->toString();
+                    }else{
+                        echo 'Ya existe proveedor con el id: ' . $_POST['id'];
+                    }
                 }else{
                     echo 'Debe ingresar datos del proveedor';
                 }
@@ -36,7 +53,14 @@
             }
         }
 
-        
+        public static function existeID( $id ){
+            $proveedores = Archivo::leerProveedores();
+            foreach( $proveedores as $prov ){
+                if( strcasecmp($prov->id, $id) == 0 )
+                    return true;
+            }
+            return false;
+        }
 
     }
 ?>
